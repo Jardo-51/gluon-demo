4bc5dd353c4104205e38ffd04a95ec8eb6822a35
Jaroslav Brtis <jaroslav.brtis@gmail.com>
2024-10-24 21:37:51 +0200

    Added combo box functionality.

--- src/main/java/com/jardoapps/gluon/demo/controller/MainController.java
+++ src/main/java/com/jardoapps/gluon/demo/controller/MainController.java
@@ -1,5 +1,21 @@
 package com.jardoapps.gluon.demo.controller;
 
-public class MainController {
+import java.net.URL;
+import java.util.ResourceBundle;
+
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.ComboBox;
+
+public class MainController implements Initializable {
+
+	@FXML
+	private ComboBox<String> demoCmb;
+
+	@Override
+	public void initialize(URL location, ResourceBundle resources) {
+		demoCmb.getItems().add("Item 1");
+		demoCmb.getItems().add("Item 2");
+	}
 
 }
